fef6b907279f06f93caba958e40e14f6d60de3a3
appsmark <martin.keesen@gmail.com>
2021-01-05 16:16:03 +0100

    Add project files.

---
+++ Press4Color.Android/SplashScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Press4Color.Droid
+{
+    [Activity(Theme = "@style/Splash", MainLauncher = true, NoHistory = true, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { InitializeApp(); });
+            startupWork.Start();
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+
+        // Initialize app while showing the splash screen
+        async void InitializeApp()
+        {
+            await Task.Delay(100); // Simulate a bit of startup work.
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
---
+++ Press4Color/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Press4Color
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly Random rnd = new Random();
+        private int color = 2;
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void PressMeButton_Pressed(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            int new_color = rnd.Next(1, 8);
+            while (new_color == color)
+            {
+                new_color = rnd.Next(1, 8);
+            }
+            color = new_color;
+            switch (color)
+            {
+                case 1:
+                    button.BackgroundColor = Xamarin.Forms.Color.Aqua;
+                    break;
+                case 2:
+                    button.BackgroundColor = Xamarin.Forms.Color.Red;
+                    break;
+                case 3:
+                    button.BackgroundColor = Xamarin.Forms.Color.Green;
+                    break;
+                case 4:
+                    button.BackgroundColor = Xamarin.Forms.Color.Blue;
+                    break;
+                case 5:
+                    button.BackgroundColor = Xamarin.Forms.Color.Yellow;
+                    break;
+                case 6:
+                    button.BackgroundColor = Xamarin.Forms.Color.Violet;
+                    break;
+                case 7:
+                    button.BackgroundColor = Xamarin.Forms.Color.Cyan;
+                    break;
+                case 8:
+                    button.BackgroundColor = Xamarin.Forms.Color.Brown;
+                    break;
+                default:
+                    color = 0;
+                    button.BackgroundColor = Xamarin.Forms.Color.White;
+                    break;
+            }
+        }
+    }
+}
